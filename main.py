c3a9daa6bdb7bf9a3a0681e5a4eead0e92770a75
cvats74 <63123314+cvats74@users.noreply.github.com>
2020-05-01 03:38:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Design(BoxLayout):
+    
+
+    def change_text(self,inputText):
+        inputText.text="by"
+
+
+class ChatApp(App):
+    def build(self):
+        return Design()
+
+
+ChatApp().run()
 
